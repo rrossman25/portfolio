4c4202d4154d1723984e1f5444144bda79559aa2
Rebecca Rossman <rrossman25@gmail.com>
2019-11-19 15:33:10 -0500

    react framework

---
+++ src/app.js
@@ -0,0 +1,15 @@
+
+import React, { Component} from 'react';
+import './app.css';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>Rebecca Rossman Portfolio</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
